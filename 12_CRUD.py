c42b4d69e4998c77a7728c34706c740bc4864580
Rosita Torres <rosamtorres4492@gmail.com>
2024-12-21 13:34:56 -0500

    Se actualizo el programa crud alumnos

--- 12_CRUD.py
+++ 12_CRUD.py
@@ -31,21 +31,55 @@ while opcion < 5:
         print('-' * ANCHO)
         print(' '* 10 + "1. Registrar alumno")
         print('-' * ANCHO)    
+
+        dni = input("Ingrese DNI: ")
+        nombre = input("Ingrese nombre: ")
+        email = input("Ingrese email: ") 
+
+        dic_nuevo_alumno = {
+            dni: {'nombre': nombre,
+            'email': email}
+        }
+
+        dic_alumnos.update(dic_nuevo_alumno)
     
     elif opcion == 2:   
         print('-' * ANCHO)
         print(' '* 10 + "2. Mostrar alumno")
         print('-' * ANCHO)  
 
+        for i, (dni, datos) in enumerate(dic_alumnos.items(), start=1):
+            print('-' * ANCHO)
+            print(f"Alumno #{i}")
+            print(f"DNI: {dni}")
+            print(f"Nombre: {datos['nombre']}")
+            print(f"Email: {datos['email']}")
+            print('-' * ANCHO)
+        input("Presione Enter para continuar...")
     elif opcion == 3:   
         print('-' * ANCHO)
         print(' '* 10 + "3. Actualizar alumno")
         print('-' * ANCHO)  
 
+        dni = input("Ingrese DNI del alumno a actualizar: ")
+        nombre = input("Ingrese nuevo nombre: ")
+        email = input("Ingrese nuevo email: ")
+
+        dic_alumnos[dni]['nombre'] = nombre
+        dic_alumnos[dni]['email'] = email
+
+        input("Se registrò correctamente, Presione Enter para continuar...")
+
     elif opcion == 4:   
         print('-' * ANCHO)
         print(' '* 10 + "4. Eliminar alumno")
         print('-' * ANCHO)  
+
+        dni = input("Ingrese DNI del alumno a eliminar: ")
+        dic_alumnos.pop(dni)
+
+        input("Se eliminò correctamente, Presione Enter para continuar...")
+
 
     elif opcion == 5:   
         print('-' * ANCHO)
